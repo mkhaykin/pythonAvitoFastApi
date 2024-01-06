66ec7f70aa673156ed7e8d209c502f74736467a1
mikhail khaykin <mkhaykin@outlook.com>
2024-01-06 10:41:43 +0300

    test: add database

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,55 @@
-from collections.abc import AsyncGenerator
+from collections.abc import AsyncGenerator, Generator
 
+import pytest
 import pytest_asyncio
 from httpx import AsyncClient
+from sqlalchemy_utils.functions import (  # type: ignore
+    create_database,
+    database_exists,
+    drop_database,
+)
 
+from api.database import get_db
 from main import app
 
+from .database import (
+    SQLALCHEMY_DATABASE_TEST_URL,
+    TestingSession,
+    create_tables,
+    drop_tables,
+    engine_test_async,
+    override_get_db,
+)
 
-@pytest_asyncio.fixture(scope="session")
+
+@pytest_asyncio.fixture(scope="function")
 async def async_client() -> AsyncGenerator:
     async with AsyncClient(app=app, base_url="http://test") as client:
         yield client
+
+
+@pytest.fixture(scope="session")
+def _create_db() -> Generator:
+    app.dependency_overrides[get_db] = override_get_db
+    if not database_exists(SQLALCHEMY_DATABASE_TEST_URL):
+        create_database(SQLALCHEMY_DATABASE_TEST_URL)
+    yield
+    drop_database(SQLALCHEMY_DATABASE_TEST_URL)
+
+
+@pytest.fixture(scope="session")
+def _create_tables(_create_db) -> Generator:  # noqa: ANN001, U101
+    create_tables()
+    yield
+    drop_tables()
+
+
+@pytest_asyncio.fixture(scope="function")
+async def async_db_engine(_create_tables) -> AsyncGenerator:  # noqa: ANN001, U101
+    yield engine_test_async
+
+
+@pytest_asyncio.fixture(scope="function")
+async def async_db(async_db_engine) -> AsyncGenerator:  # noqa: ANN001, U100
+    async with TestingSession() as session:
+        yield session
---
+++ tests/database.py
@@ -0,0 +1,70 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.engine import URL, create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm.session import sessionmaker
+
+from api.config import settings
+from api.database import Base
+
+
+SQLALCHEMY_DATABASE_TEST_URL = URL.create(
+    drivername='postgresql',
+    username=settings.POSTGRES_USER,
+    password=settings.POSTGRES_PASSWORD,
+    host=settings.POSTGRES_HOST,
+    port=settings.POSTGRES_PORT,
+    database=f'{settings.POSTGRES_DB}_test',
+)
+
+SQLALCHEMY_DATABASE_TEST_URL_async = URL.create(
+    drivername='postgresql+asyncpg',
+    username=settings.POSTGRES_USER,
+    password=settings.POSTGRES_PASSWORD,
+    host=settings.POSTGRES_HOST,
+    port=settings.POSTGRES_PORT,
+    database=f'{settings.POSTGRES_DB}_test',
+)
+
+engine_test_async = create_async_engine(
+    SQLALCHEMY_DATABASE_TEST_URL_async,
+    pool_pre_ping=True
+)
+
+TestingSession = sessionmaker(
+    bind=engine_test_async,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autocommit=False,
+    autoflush=False,
+)    # type: ignore
+
+
+async def override_get_db() -> AsyncGenerator:
+    async with TestingSession() as session:
+        yield session
+
+
+async def create_tables_async():
+    async with engine_test_async.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def drop_tables_async():
+    async with engine_test_async.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+# SYNC
+engine_test = create_engine(
+    SQLALCHEMY_DATABASE_TEST_URL,
+    pool_pre_ping=True,
+)
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine_test)
+
+
+def drop_tables():
+    Base.metadata.drop_all(bind=engine_test)
