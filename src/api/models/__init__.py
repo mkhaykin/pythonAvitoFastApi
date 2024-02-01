54371c07ddf6a5c0d3a065891bc526421e9b808a
mikhail khaykin <mkhaykin@outlook.com>
2024-02-01 10:24:14 +0300

    fix: models import *

--- src/api/models/__init__.py
+++ src/api/models/__init__.py
@@ -1,5 +1,13 @@
 from .base import Base
 from .query import Query
 from .stat import Stat
+from .crud import query_add, query_get, queries_get
 
-__all__ = ["Base", "Query", "Stat"]
+__all__ = [
+    "Base",
+    "Query",
+    "Stat",
+    "query_add",
+    "query_get",
+    "queries_get",
+]
